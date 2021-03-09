ff555d53638f82b57eba2aed67dd3cb9320f05e3
dolapo-oluremi <62415331+dolapo-oluremi@users.noreply.github.com>
2021-03-09 18:53:44 -0500

    text font, color and brand edit
    edited css, footer, text styles and color.

--- build/main.js
+++ build/main.js
@@ -4,9 +4,18 @@ window.addEventListener('load', () =>{
         menuIcon.classList.toggle('change')
     })
 
+    const buyBtn = document.getElementsByClassName('buyBtn');
+  for(var i = 0; i < buyBtn.length; i++){
+      const thisBuyBtn = buyBtn[i]
+    thisBuyBtn.addEventListener('click', function(event){
+        const thisItem = event.target;
+        const thisItemName = thisItem.parentElement.firstChild.innerText;
+        const thisItemPrice =  thisItem.parentElement.children[2].innerText.slice(1, 5);
 
-
-    
+        
+        console.log(thisItemPrice);
+    })
+  }
 
 
 
